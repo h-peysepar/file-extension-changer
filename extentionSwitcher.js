0e13cd55cf0a3b6cab9f30a9918aee2012cf05c7
HadiPr <hadipeysepar79@gmail.com>
2023-04-10 17:21:04 +0330

    improve pattern and convert js to ts

--- extentionSwitcher.js
+++ extentionSwitcher.js
@@ -1,31 +1,33 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const exceptions = ['node_modules']
-const sources = ['ts', 'tsx']
-changeExtension(__dirname)
-function changeExtension(dir){
-     const filesAndDirs = fs.readdirSync(dir)
-     for(let item of filesAndDirs){
-          if(exceptions.includes(item)){
-               continue;
-          }
-          try {
-               const path = path.join(dir, item)
-               fs.readdirSync(path) //throw error if path is not a directory
-               changeExtension(path)
-          } catch (_) {
-               for(exception of exceptions){
-                    if(item.slice(-(exception.length)) === exception){
-                         fs.renameSync(path.join(dir, item), path.join(dir, item.slice(0, item.length - 2) + 'js'))
-                    }
-               }
-               if(item.slice(-2) === 'ts'){
-                    fs.renameSync(path.join(dir, item), path.join(dir, item.slice(0, item.length - 2) + 'js'))
-               }
-               if(item.slice(-3) === 'tsx'){
-                    fs.renameSync(path.join(dir, item), path.join(dir, item.slice(0, item.length - 3) + 'js'))
-               }
-          }
-     }
+const exceptions = ['node_modules'];
+const dir = ''; // fill absoulte path of directory here
+changeExtension(dir || __dirname);
+
+function changeExtension(dir) {
+  const filesAndDirs = fs.readdirSync(dir);
+  for (let item of filesAndDirs) {
+    if (exceptions.includes(item)) {
+      continue;
+    }
+    try {
+      const pathtarget = path.join(dir, item);
+      fs.readdirSync(pathtarget); //throw error if pathtarget is not a directory
+      changeExtension(pathtarget);
+    } catch (_) {
+      if (item.slice(-2) === 'js') {
+        fs.renameSync(
+          path.join(dir, item),
+          path.join(dir, item.slice(0, item.length - 2) + 'ts')
+        );
+      }
+      if (item.slice(-3) === 'jsx') {
+        fs.renameSync(
+          path.join(dir, item),
+          path.join(dir, item.slice(0, item.length - 3) + 'tsx')
+        );
+      }
+    }
+  }
 }
